16659c346bb97e25cfae1b615a93eca702d42588
leg-jules <149069485+leg-jules@users.noreply.github.com>
2024-10-25 10:09:35 +0200

    Add files via upload
    Base du projet depuis mr flasque

---
+++ main(1).c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "map.h"
+
+int main() {
+    t_map map;
+
+    // The following preprocessor directive checks if the code is being compiled on a Windows system.
+    // If either _WIN32 or _WIN64 is defined, it means we are on a Windows platform.
+    // On Windows, file paths use backslashes (\), hence we use the appropriate file path for Windows.
+#if defined(_WIN32) || defined(_WIN64)
+    map = createMapFromFile("..\\maps\\example1.map");
+#else
+    map = createMapFromFile("../maps/example1.map");
+#endif
+
+    printf("Map created with dimensions %d x %d\n", map.y_max, map.x_max);
+    for (int i = 0; i < map.y_max; i++)
+    {
+        for (int j = 0; j < map.x_max; j++)
+        {
+            printf("%d ", map.soils[i][j]);
+        }
+        printf("\n");
+    }
+    // printf the costs, aligned left 5 digits
+    for (int i = 0; i < map.y_max; i++)
+    {
+        for (int j = 0; j < map.x_max; j++)
+        {
+            printf("%-5d ", map.costs[i][j]);
+        }
+        printf("\n");
+    }
+    displayMap(map);
+    return 0;
+}
