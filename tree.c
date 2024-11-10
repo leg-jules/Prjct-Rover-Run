e831f689c0847a7beb9126ee72817be1e9c18f42
leg-jules <jules.legalais@efrei.net>
2024-11-10 16:54:40 +0100

    restucturation et fonction : freeNode availsMinParent createNode freeTree

--- tree.c
+++ tree.c
@@ -4,36 +4,76 @@
 
 #include "tree.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 int moveToDoleft = 5;
 
+void freeNode(t_node *root){
+    if (root == NULL)
+    {
+        return;
+    }
+    for (int i = 0; i < root->nbSons; i++)
+    {
+        freeNode(root->sons[i]);
+    }
+    free(root->sons);
+    free(root);
+    printf("Node freed successfully \n");
+}
+
+int* availsMinParent(int* list, int size, int val) {
+    int *newList = (int *) malloc(size * sizeof(int));
+    if (newList == NULL) {
+        printf("Erreur d'allocation de mémoire\n");
+        return NULL;
+    }
+    int j = 0;
+    for (int i = 0;
+         i < size;
+         i++) {
+        if (list[i] != val) {
+            newList[j] = list[i];
+            j++;
+        }
+    }
+    return newList;
+}
+
 t_node* createNode(int value, int depth, int nbSons){
     t_node *node = (t_node*)malloc(sizeof(t_node));
+    if (node == NULL) //libère l'espace si la création du noeud échoue
+    {
+        printf("Erreur d'allocation de mémoire\n");
+        return NULL;
+    }
+
     node->value = value;
     node->depth = depth;
     node->nbSons = nbSons;
-    for (int i = 0; i < MAX_CHILDREN; i++)
+    node->sons = (t_node**)malloc(nbSons * sizeof(t_node*));
+    int* new_avails = availsMinParent(node->sons, nbSons, value);
+    node->availableSons = *new_avails;
+    if (node->sons == NULL)    //libère l'espace si la création des fils échoue
     {
+        free(node);
+        return NULL;
+    }
+    for (int i = 0; i < nbSons; i++) {
         node->sons[i] = NULL;
     }
     return node;
 }
 
-//fonction noeud----------------------------------------------------------------------------------------------------------------
-
-void addChild(t_node *parent, t_node *child);
-t_node *minCostWay(t_node *root, int *minCost);
-void freeNode(t_node *root);
-t_node printNode(t_node node);
-
-//fonction arbre----------------------------------------------------------------------------------------------------------------
-
-t_tree createTree(){
-    t_tree tree;
-    tree.root = NULL;
-    return tree;
+void freeTree(t_tree *tree) {
+    if (tree == NULL) {
+        printf("Tree is NULL\n");
+        return;
+    }
+    else{
+        freeNode(tree->root);
+        free(tree);
+        printf("Tree freed\n");
+    }
+    printf("Tree freed failed\n");
 }
-
-void freeTree(t_tree *tree);
-
-void printTree(t_tree tree);
--- tree.h
+++ tree.h
@@ -9,26 +9,31 @@
 typedef struct s_node { //noeud
     int value;
     int depth;
-    struct s_treeNode **sons[MAX_CHILDREN]; //changer max_children après
     int nbSons;
-    int avails; //tab de possibilité
+    int availableSons; //tab de possibilité
+    struct s_node **sons; //changer max_children après
 } t_node;
 
 typedef struct tree { // arbre
     t_node *root;
 } t_tree;
 
-//fonctions de noeud
+////fonctions de noeud
+
 t_node* createNode(int value, int depth, int nbSons);
+
 void freeNode(t_node *root);
-void addChild(t_node *parent, t_node *child);
-t_node *minCostWay(t_node *root, int *minCost);
-t_node printNode(t_node node);
+//void addChild(t_node *parent, t_node *child);
+//t_node *minCostWay(t_node *root, int *minCost);
+//t_node printNode(t_node node);
 
-//fonctions d'arbre
-t_tree createTree();
+////fonctions d'arbre
+//t_tree createTree();
+
 void freeTree(t_tree *tree);
-void printTree(t_tree tree);
 
+int* availsMinParent(int* list, int size, int val);
+
+//void printTree(t_tree tree);
 
 #endif //UNTITLED1_TREE_H
