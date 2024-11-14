718872bfa82043ac45e573712e338f63953c25f3
RaphaelBilly <raphael.billy@efrei.net>
2024-11-14 11:54:30 +0100

    added printNTree function

---
+++ tree.c
@@ -0,0 +1,77 @@
+#include "tree.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int *removeFromList(int *list, int val, int len_list){
+
+    int* new_list = (int*)malloc(sizeof(int)*(len_list - 1));
+    int i, j;
+    j = 0;
+    for (i = 0; i <= len_list; i++) {
+        if (val != list[i]) {
+            new_list[j] = list[i];
+            j++;
+        }
+    }
+    return new_list;
+}
+
+
+t_node *createNode(int val, int mvt, int nd_sons, int* list_choix, int depth) {
+    t_node *new_node;
+    new_node = (t_node *)malloc(sizeof(t_node));
+
+    new_node->value = val;
+    new_node->id_mouvement = mvt;
+    new_node->ndSons = nd_sons;
+    new_node->sons = (t_node **)malloc(nd_sons*sizeof(t_node *));
+    int *new_avails = removeFromList(list_choix, val, nd_sons);//on enlève le mouvement du noeud actuel (son id_mouvement) à sa liste
+    new_node->avails = new_avails; //tous les choix possibles qui dépend du noeud parent de ce nouveau noeud (si noeud = racine, initialisation du robot, liste complète)
+    new_node->depth = depth;
+
+    for(int i = 0; i < nd_sons; i++){
+        new_node->sons[i]=NULL;
+    }
+    return new_node;
+}
+
+
+
+
+t_tree createNTree(t_node* node) {
+
+    if (node->depth < 5) {
+        int i;
+        for (i = 0; i < node->ndSons; i++) {
+            t_node *new_son = createNode(node->avails[i], node->avails[i], node->ndSons - 1 , node->avails, node->depth + 1);
+            node->sons[i] = new_son;
+            createNTree(new_son);
+        }
+    }
+
+    t_tree tree;
+    tree.root = node;
+    return tree;
+}
+
+
+
+void printNTree(t_tree tree) {
+    if (tree.root == NULL) return;
+
+    void printNode(t_node* node, int depth, char* prefix) {
+        if (depth >= 3) return;
+
+        printf("%s", prefix);
+        printf("%s", (depth == 0) ? "" : "|-- ");
+        printf("Node value: %d\n", node->value);
+
+        char new_prefix[256];
+        snprintf(new_prefix, sizeof(new_prefix), "%s%s", prefix, (depth == 0) ? "" : "|   ");
+        for (int i = 0; i < node->ndSons; i++) {
+            printNode(node->sons[i], depth + 1, new_prefix);
+        }
+    }
+
+    printNode(tree.root, 0, "");
+}
---
+++ tree.h
@@ -0,0 +1,27 @@
+#ifndef TREE_H
+#define TREE_H
+
+typedef struct s_node
+{
+    int value;
+    int id_mouvement;
+    int depth;
+    struct s_node **sons; //tableau de pointeur
+    int ndSons; //taille physique du tableau
+    int *avails; //choix restants du noeud
+}t_node;
+
+int *removeFromList(int*, int, int);
+t_node *createNode(int val, int mvt, int nd_sons, int* list_choix, int depth);
+
+
+
+typedef struct n_tree {
+    t_node* root;
+} t_tree;
+
+t_tree createNTree(t_node *node); // A modifier : Changer la fonction pour insertIntoNTree
+void printNTree(t_tree tree);
+
+
+#endif //TREE_H
